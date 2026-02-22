3d19e6543d094406c5da11812cbe2e9c809cc9e5
christofera1994 <christofera1994@gmail.com>
2026-02-22 16:10:02 +0100

    Delete js replit directory

--- js replit/config.js
+++
@@ -1,5 +0,0 @@
-// Supabase Configuration
-const SUPABASE_CONFIG = {
-  url: 'https://yqjyytwxvkwbxsaygttd.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlxanl5dHd4dmt3YnhzYXlndHRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MDY0NjEsImV4cCI6MjA4NzE4MjQ2MX0.1H1k5k1CZ-DniW-uLEKhtLsUnBNbJWBlBDeorbe5cnY'
-};
